098785b4ae62dd82a76642487939715de4420390
MaxTheFirst <80311961+MaxTheFirst@users.noreply.github.com>
2023-05-14 16:05:52 +0300

    decorator

--- calc.py
+++ calc.py
@@ -17,6 +17,17 @@ def all_replace(eq: str):
         eq = eq.replace(*arg)
     return eq
 
+def iter_re(func):
+    def wrapper(eq: str, pattern, arg_pattern, *args, **kwargs):
+        while True:
+            part = re.search(pattern, eq)
+            if not part:
+                return eq
+            arg = re.search(arg_pattern, part.group()).group()
+            new_replace = func(arg, *args, **kwargs)
+            eq =  eq[:part.start()] + new_replace + eq[part.end():]
+    return wrapper
+
 def replace_roots(eq: str):
     for key, val in enumerate(replacements.ROOTS):
         parts = re.findall(patterns.ROOT[0].format(val), eq)
@@ -25,18 +36,18 @@ def replace_roots(eq: str):
             eq = eq.replace(part, patterns.ROOT[2].format(key+2, arg), 1)
     return eq
 
-def replace_re(eq: str, pattert, arg_pattern, new):
-    parts = re.findall(pattert, eq)
-    for part in re.finditer(pattert, eq):
-        arg = re.findall(arg_pattern, part.group())[0]
-        if '&' in arg:
-            arg = arg.split('&')
-        elif ' ' in arg:
-            arg = arg.split(' ')
-        else:
-            arg = [arg]
-        eq =  eq[:part.start()] + new.format(*arg[::-1]) + eq[part.end():]
-    return eq
+@iter_re
+def replce_root()
+
+@iter_re
+def replace_re(new):
+    if '&' in arg:
+        arg = arg.split('&')
+    elif ' ' in arg:
+        arg = arg.split(' ')
+    else:
+        arg = [arg]
+    return new.format(*arg[::-1])
 
 def replaces_re(eq: str, index = None):
     for pattert, arg_pattern, new in patterns.REP_RE[1::]:
